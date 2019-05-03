7dffadd0a0877183ea2c1f35fe8d8ad81e5b2bda
sw-son <tkddn3001@gmail.com>
2019-05-03 18:10:58 +0900

    데이터베이스 모델 및 import

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Object(models.Model):
+    Index = models.AutoField(primary_key=True)
+    Type = models.IntegerField(null=True, blank=True)
+    Related = models.IntegerField(null=True, blank=True)
+    Name = models.CharField(max_length=20)
+
+class Attribute(models.Model):
+    Index = models.AutoField(primary_key=True)
+    Value = models.CharField(max_length=5)
+
+class Property(models.Model):
+    Index = models.AutoField(primary_key=True)
+    Object = models.IntegerField(null=True, blank=True)
+    Attribute = models.IntegerField(null=True, blank=True)    
--- api/views.py
+++ api/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
-
+from .models import Object, Attribute, Property
 # Create your views here.
