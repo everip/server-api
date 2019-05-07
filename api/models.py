96e9a0cbb9194810a621a084885b80ac93d5782b
yonghyunkim <sociablesasha@gmail.com>
2019-05-07 10:19:23 +0900

    [API] 모델 foreignKey 추가

--- api/models.py
+++ api/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Object(models.Model):
     Index = models.AutoField(primary_key=True)
-    Type = models.IntegerField(null=True, blank=True)
-    Related = models.IntegerField(null=True, blank=True)
-    Name = models.CharField(max_length=20)
+    Type = models.IntegerField(null=True)
+    Related = models.ForeignKey(Object, on_delete=models.CASCADE, null=True)
+    Name = models.CharField(max_length=100)
+
 
 class Attribute(models.Model):
     Index = models.AutoField(primary_key=True)
-    Value = models.CharField(max_length=5)
+    Value = models.CharField(max_length=10)
+
 
 class Property(models.Model):
     Index = models.AutoField(primary_key=True)
-    Object = models.IntegerField(null=True, blank=True)
-    Attribute = models.IntegerField(null=True, blank=True, max_length=5)    
+    Object = models.ForeignKey(Object, on_delete=models.CASCADE)
+    Attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
