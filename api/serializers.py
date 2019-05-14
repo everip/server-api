722fad835312c6976617ae7e6dacd79ad61d26f3
sw-son <tkddn3001@gamil.com>
2019-05-15 02:53:53 +0900

    [API] views.py continent함수 작성 및 테스트

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Object, Attribute, Property
-
-
-class ObjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Object
-        fields = ('Index','Type','Related','Name')
--- api/views.py
+++ api/views.py
@@ -1,14 +1,47 @@
 import json
 import requests
-
 from django.shortcuts import render
 from .models import Object, Attribute, Property
 from django.http import HttpResponse, JsonResponse
-from rest_framework import viewsets
-from .serializers import ObjectSerializer
+
+
 # Create your views here.
 
-class ObjectSerializer(viewsets.ModelViewSet):
-    queryset = Object.objects.all()
-    serializers_class = ContinentSerializer
+def continent(request):
 
+    if '' == request.GET.get('value', ''):
+        return HttpResponse(status=400)
+
+    value = request.GET['value']
+    data = {}
+    try:
+        data = []
+        if value == 'continents':
+            object_data = Object.objects.all()
+            lenod = len(object_data)
+            for i in range(lenod):
+                property_data = Property.objects.filter('index' = object_data[i][index])
+                attribute_data = Attribute.objects.filter('index' = property_data[i][attribute])
+                data.append({
+                    "name" : object_data[i][name],
+                    "attribute" : attribute_data[i][value]
+                })            
+
+
+            
+        else:
+            object_data = Object.objects.filter('name' = value)
+            property_data = Property.objects.filter('index' = object_data[0][index])
+            attribute_data = Attribute.objects.filter('index' = property_data[0][attribute])
+            data.append({
+                "name" : object_data[0][name],
+                "attributes" : attribute_data[0][value]
+            })
+        return JsonResponse(
+            data,
+            safe=False,
+            json_dumps_params={'ensure_ascii': False}
+        )
+    except:
+        return HttpResponse(statsu=500)
+    
